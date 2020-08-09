c70cd9485a1cd9eee6078d095ef3c74704722812
khadeer <shaikkafil01@gmail.com>
2020-08-09 23:31:02 +0300

    Add files via upload

---
+++ khadeerbasha/hr_batch_payroll_direct/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Hr Direct Batch Payroll',
+    'version': '1.0',
+    'category': 'Human Resources',
+	'author' : 'me',
+    'description': """Hr Direct Batch Payroll""",
+    'depends': ['hr_payroll'],
+    'website':'http://www.me.com/',
+    'data': ['views/hr_direct_view.xml'],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+}
